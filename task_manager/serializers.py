02ea963fd37d5c5dbad54f1ba05556d8d2bcdbea
Izzet <yunusovizzet6@gmail.com>
2025-04-24 19:42:17 +0200

    hw13

--- task_manager/serializers.py
+++ task_manager/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from task_manager.models import Task
-
+from task_manager.models import Task, SubTask, Category
+from django.utils import timezone
 
 # HW12. Задание 1: Эндпоинт для создания задачи
 # Задача должна быть создана с полями title, description, status, и deadline.
@@ -21,3 +21,76 @@ class TaskByIDSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
+
+
+
+# HW 13
+
+# Задание 1: Переопределение полей сериализатора
+class SubTaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTask
+        fields = [
+            "id",
+            "title",
+            "description",
+            "task",
+            "status",
+            "deadline",
+            "created_at"
+        ]
+        read_only_fields = ['created_at',]
+
+
+# Задание 2: Переопределение методов create и update
+class CategoryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Category
+        fields= ["name"]
+
+    def validate_name(self, value: str):
+        if value:
+            unique_name = Category.objects.filter(name__iexact=value).exists()
+            if unique_name:
+                raise serializers.ValidationError(
+                    "Такая категория уже существует!"
+                )
+        return value
+
+
+# Задание 3: Использование вложенных сериализаторов
+class SubTaskSerializer(serializers.ModelSerializer):
+    task = serializers.StringRelatedField()
+
+    class Meta:
+        model = SubTask
+        fields = "__all__"
+
+
+class TaskDetailSerializer(serializers.ModelSerializer):
+    subtasks = SubTaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+        # exclude = ['']  # все поля, кроме...
+
+
+# Задание 4: Валидация данных в сериализаторах
+class TaskCreateSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = [
+            "title",
+            "description",
+            "categories",
+            "deadline"
+        ]
+
+
+    def validate_deadline(self, value: str):
+        if value < timezone.now():
+            raise serializers.ValidationError(
+                f"Дата не может быть меньше {timezone.now()}"
+            )
+        return value
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,49 +1,250 @@
+from django.db.models import Count, QuerySet
+from django.db.models.functions import ExtractWeekDay
+from django.http import HttpResponse
+from django.utils import timezone
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from .serializers import TaskCreateSerializer, TaskListSerializer
-from task_manager.models import Task
-from django.db.models import Count
-from django.http import JsonResponse
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
-@api_view(['POST'])
-def task_create(request):
-    serializer = TaskCreateSerializer(data=request.data)
+from task_manager.models import Task, SubTask
+from task_manager.serializers import (TaskCreateSerialize,
+                                      TaskDetailSerializer,
+                                      SubTaskCreateSerializer,
+                                      SubTaskSerializer)
 
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data,status=201)
-    else:
-        return Response(serializer.errors,status=404)
 
+# HW13
+# Задание 5: Создание классов представлений
+class SubTaskListCreateAPIView(APIView, PageNumberPagination):
+    page_size = 5
+
+    def get_queryset(self, request: Request):
+
+        queryset: QuerySet[SubTask] = SubTask.objects.all()
+
+        tasks = request.query_params.getlist('task')
+        status = request.query_params.get('status')
+
+        if tasks:
+            queryset = queryset.filter(task__title__in=tasks)
+
+        if status:
+            valid_statuses = [choice[0] for choice in SubTask.STATUS_CHOICES]
+            if status in valid_statuses:
+                queryset = queryset.filter(status=status)
+            else:
+                queryset = queryset.none()
+
+        return queryset.order_by("-created_at")
+
+    def get_page_size(self, request):
+        page_size = request.query_params.get('page_size')
+
+        if page_size and page_size.isdigit():
+            return int(page_size)
+
+        return self.page_size
+
+    def get(self, request: Request) -> Response:
+        subtasks: QuerySet[SubTask] = self.get_queryset(request=request)
+        # subtasks: QuerySet[SubTask] = SubTask.objects.all().order_by("-created_at")
+        results = self.paginate_queryset(queryset=subtasks, request=request, view=self)
+        serializer = SubTaskSerializer(results, many=True)
+        # return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return self.get_paginated_response(data=serializer.data)
+
+    def post(self, request: Request) -> Response:
+        serializer = SubTaskCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SubTaskDetailUpdateDeleteView(APIView):
+    def get(self, request: Request, **kwargs) -> Response:
+        try:
+            subtask = SubTask.objects.get(id=kwargs['subtask_id'])
+        except SubTask.DoesNotExist:
+            return Response(
+                data={
+                    "message": "Подзадача не найдена!"
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = SubTaskSerializer(subtask)
+
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def put(self, request: Request, **kwargs) -> Response:
+        try:
+            subtask = SubTask.objects.get(id=kwargs['subtask_id'])
+        except SubTask.DoesNotExist:
+            return Response(
+                data={
+                    "message": "Подзадача не найдена!"
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = SubTaskCreateSerializer(instance=subtask, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                data=serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        else:
+            return Response(
+                data=serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def delete(self, request: Request, **kwargs) -> Response:
+        try:
+            subtask = SubTask.objects.get(id=kwargs['subtask_id'])
+        except SubTask.DoesNotExist:
+            return Response(
+                data={
+                    "message": "Подзадача не найдена!"
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        subtask.delete()
+
+        return Response(
+            data={
+                "message": "Книга была успешно удалена."
+            },
+            status=status.HTTP_202_ACCEPTED
+        )
+
+
+def user_hello1(request):
+    return HttpResponse(
+        f"<h1>Hello, Prog2!!! :)</h1>"
+    )
+
+
+class TaskListCreateAPIView(APIView):
+    WEEKDAY_MAP = {
+        'воскресенье': 1,
+        'sunday': 1,
+        'понедельник': 2,
+        'monday': 2,
+        'вторник': 3,
+        'tuesday': 3,
+        'среда': 4,
+        'wednesday': 4,
+        'четверг': 5,
+        'thursday': 5,
+        'пятница': 6,
+        'friday': 6,
+        'суббота': 7,
+        'saturday': 7,
+    }
+
+    def get_queryset(self, request: Request) -> QuerySet[Task]:
+        queryset: QuerySet[Task] = Task.objects.all()
+        day_of_week = request.query_params.get("weekday")
+
+        if day_of_week:
+            if day_of_week not in self.WEEKDAY_MAP:
+                raise ValidationError("Неверный день недели!")
+
+            weekday_num = self.WEEKDAY_MAP.get(day_of_week.lower())
+            queryset = queryset.annotate(weekday=ExtractWeekDay('deadline')).filter(weekday=weekday_num)
+
+        return queryset
+
+    def get(self, request: Request) -> Response:
+        tasks = self.get_queryset(request=request)
+        serializer = TaskDetailSerializer(tasks, many=True)
+
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def post(self, request: Request) -> Response:
+        serializer = TaskCreateSerialize(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['POST'])
+# def tasks_create(request: Request) -> Response:
+#     serializer = TaskCreateSerialize(data=request.data)
+#
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     else:
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+
+# @api_view(['GET'])
+# def list_of_tasks(request) -> Response:
+#     tasks = Task.objects.all()
+#     serializer = TaskDetailSerializer(tasks, many=True)
+#
+#     return Response(
+#         data=serializer.data,
+#         status=status.HTTP_200_OK
+#     )
 
 @api_view(['GET'])
-def task_list(request):
-    tasks = Task.objects.all()
-    serializer = TaskListSerializer(tasks, many=True)
-    return Response(serializer.data, status=200)
-
-
-@api_view(['GET'])
-def task_detail(request, task_id):
+def get_task_by_id(request, task_id: int) -> Response:
     try:
         task = Task.objects.get(id=task_id)
     except Task.DoesNotExist:
         return Response(
-            {"message": "Task not found"},
+            data={
+                "message": "TASK NOT FOUND"
+            },
+            status=404
         )
 
-    serializer = TaskListSerializer(task)
-    return Response(serializer.data, status=200)
+    serializer = TaskDetailSerializer(task)
+
+    return Response(
+        data=serializer.data,
+        status=200
+    )
 
 
-def task_statistic(request):
-    count_task = Task.objects.aggregate(total_tasks=Count('id'))
-    count_task_by_status = Task.objects.values('status').annotate(task_count=Count('id'))
-    status_counts = {item['status']: item['task_count'] for item in count_task_by_status}
-    pending_tasks = Task.objects.filter(status='pending').count()
-    response_data = {
-        'count_task': count_task['total_tasks'],
-        'pending_tasks': pending_tasks,
-        'count_task_by_status': status_counts
-    }
-    return JsonResponse(response_data)
+@api_view(['GET'])
+def tasks_count(request) -> Response:
+    tasks_cn = Task.objects.count()
+
+    return Response(data=f"{tasks_cn=}", status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def tasks_count_by_status(request) -> Response:
+    statuses_count_by_task = Task.objects.values("status").annotate(Count("id"))
+    serializer = TaskStatusCountSerializer(statuses_count_by_task, many=True)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def tasks_of_overdue(request) -> Response:
+    count_of_overdue_task = Task.objects.filter(deadline__lt=timezone.now()).count()
+
+    return Response(data=f"{count_of_overdue_task=}", status=status.HTTP_200_OK)
